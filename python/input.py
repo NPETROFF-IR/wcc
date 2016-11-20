9a8230a2c4bee19952bd14a316237774a2e84473
Natalya812 <natalya.petroff@cuny.edu>
2016-11-19 23:50:44 -0500

    madlib etc
    more python scripts

---
+++ python/input.py
@@ -0,0 +1,27 @@
+#name = raw_input ('What is your name?')
+#print ('Hi ' + name)
+
+#name = raw_input('What is your name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age ' years old.')
+
+#name = raw_input('What is yor name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age + ' years old.')
+
+#age = raw_input('How old are you? ')
+#dog_years= int(age) * 7
+#print('You are ' + str(dog_years) + ' old in dog years.')
+
+meal = raw_input('How much was your meal? ')
+tip = float(meal) * .20
+total = float(tip) + float(meal)
+print ('You should tip $' + str(tip) + '.Your total cost would be $' + str(total))
+
+meal = float(raw_input('How much was your meal? '))
+tip = meal * .20
+total = tip + meal
+print ('You should tip $' + str(tip) + '.Your total cost would be $' + str(total))
+
+
+
